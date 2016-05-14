29efd0782ed0be4758431dc49e34cea142eb861e
Oliver Kurth <okurth@gmail.com>
2016-05-14 16:07:53 -0700

    try to prevent services from starting using policy-rc.d (doesn't work), and add a few packages

--- customize.sh
+++ customize.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo $'#!/bin/sh\nexit 101' > /usr/sbin/policy-rc.d
+sudo chmod +x /usr/sbin/policy-rc.d
+
 wget http://archive.raspbian.org/raspbian.public.key -O - | apt-key add -
 wget http://archive.raspberrypi.org/debian/raspberrypi.gpg.key -O - | apt-key add -
 apt-get update
@@ -7,10 +10,15 @@ apt-get -y upgrade
 
 apt-get install -y sudo openssh-server ntp patch less rsync vim
 apt-get install -y iw wpasupplicant hostapd dnsmasq pi-bluetooth
-apt-get install -y firmware-brcm80211 firmware-realtek
+apt-get install -y firmware-brcm80211 firmware-realtek firmware-ralink
+apt-get install -y git
+apt-get install -y python python-flask python-httplib2 python-iwlib python-netifaces
+apt-get install -y nginx-common nginx-extras uwsgi uwsgi-plugin-python
  
 adduser pi
 usermod -a -G sudo,staff,kmem,plugdev pi
 
 apt-get clean
+
+rm /usr/sbin/policy-rc.d
 
