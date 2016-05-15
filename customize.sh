c92b26b00d87cf8e2be21f224f2e20d18d5ee61f
Oliver Kurth <okurth@gmail.com>
2016-05-15 13:21:38 -0700

    add crda and use --force-yes

--- customize.sh
+++ customize.sh
@@ -5,12 +5,12 @@ wget http://archive.raspberrypi.org/debian/raspberrypi.gpg.key -O - | apt-key ad
 apt-get update
 apt-get -y upgrade
 
-apt-get install -y sudo openssh-server ntp patch less rsync vim
-apt-get install -y iw wpasupplicant hostapd dnsmasq pi-bluetooth
-apt-get install -y firmware-brcm80211 firmware-realtek firmware-ralink
-apt-get install -y git
-apt-get install -y python python-flask python-httplib2 python-iwlib python-netifaces
-apt-get install -y nginx-common nginx-extras uwsgi uwsgi-plugin-python
+apt-get install -y --force-yes sudo openssh-server ntp patch less rsync vim
+apt-get install -y --force-yes iw wpasupplicant hostapd crda dnsmasq pi-bluetooth
+apt-get install -y --force-yes firmware-brcm80211 firmware-realtek firmware-ralink
+apt-get install -y --force-yes git
+apt-get install -y --force-yes python python-flask python-httplib2 python-iwlib python-netifaces
+apt-get install -y --force-yes nginx-common nginx-extras uwsgi uwsgi-plugin-python
  
 adduser pi
 usermod -a -G sudo,staff,kmem,plugdev pi
