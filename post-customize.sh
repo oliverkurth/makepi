9223a60d8489b848b2064a7f275225cd995f837b
Oliver Kurth <okurth@gmail.com>
2016-05-16 16:10:29 -0700

    test for pypiwifi directory

--- post-customize.sh
+++ post-customize.sh
@@ -3,6 +3,11 @@
 chrootdir=$1
 pypiwifi_dir=~/pypiwifi/
 
+if [ ! -d ${pypiwifi_dir} ] ; then
+        echo "${pypiwifi_dir} does not exist - you need to clone https://github.com/oliverkurth/pypiwifi.git"
+        exit 1
+fi
+
 cp -R ${pypiwifi_dir} ${chrootdir}/home/pi
 chown -R 1000:1000 ${chrootdir}/home/pi/pypiwifi
 
