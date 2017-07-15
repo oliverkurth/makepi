a5c42f59780e8e328d25cbfd5cae4230fc1750b5
Oliver Kurth <okurth@gmail.com>
2017-07-15 12:51:14 -0700

    no pi-bluetooth in stretch

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get -y upgrade
 
 apt-get install -y --force-yes sudo openssh-server ntp patch less rsync vim parted
-apt-get install -y --force-yes iw wpasupplicant hostapd crda dnsmasq pi-bluetooth
+apt-get install -y --force-yes iw wpasupplicant hostapd crda dnsmasq
 apt-get install -y --force-yes firmware-brcm80211 firmware-realtek firmware-ralink firmware-atheros bluez-firmware
 apt-get install -y --force-yes git
 apt-get install -y --force-yes python python-flask python-httplib2 python-netifaces
