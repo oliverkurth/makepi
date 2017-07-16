b5974c730971c7bcfcf0af234a3d6d9747a10750
Oliver Kurth <okurth@gmail.com>
2017-07-16 13:42:42 -0700

    make wpa config file group writable by netdev

--- customize.sh
+++ customize.sh
@@ -32,6 +32,11 @@ useradd -m pi -s /bin/bash -p '$6$24rIum07$C07au5jT1GDCaT7QIO4QpYfMmciSTEyeuhfNo
 usermod -a -G sudo,staff,kmem,plugdev,netdev pi
 usermod -a -G netdev www-data
 
+chmod g+w /etc/wpa_supplicant/
+chmod g+w /etc/wpa_supplicant/wpa_supplicant.conf
+chgrp netdev /etc/wpa_supplicant/
+chgrp netdev /etc/wpa_supplicant/wpa_supplicant.conf
+
 # we replaced this, can't have two configs on 0.0.0.0:80
 rm -f /etc/nginx/sites-enabled/default
 
