c69f5efbee2e1463e2d9faf1d914794278646b7e
Oliver Kurth <okurth@gmail.com>
2017-07-15 19:18:37 -0700

    stretch firmware package seems broken, and we need to pull brcmfmac43430-sdio extra from github

--- customize.sh
+++ customize.sh
@@ -7,12 +7,25 @@ apt-get -y upgrade
 
 apt-get install -y --force-yes sudo openssh-server ntp patch less rsync vim parted
 apt-get install -y --force-yes iw wpasupplicant hostapd crda dnsmasq
-apt-get install -y --force-yes firmware-brcm80211 firmware-realtek firmware-ralink firmware-atheros bluez-firmware
+apt-get install -y --force-yes firmware-realtek firmware-ralink firmware-atheros bluez-firmware
 apt-get install -y --force-yes git
 apt-get install -y --force-yes python python-flask python-httplib2 python-netifaces
 apt-get install -y --force-yes python-bluez python-serial
 apt-get install -y --force-yes nginx-common nginx-extras uwsgi uwsgi-plugin-python
 apt-get install -y --force-yes ppp
+apt-get install -y --force-yes curl
+
+(
+cd /var/cache/apt/archives
+[ -f firmware-brcm80211_0.43_all.deb ] || \
+	curl -O http://archive.raspbian.org/raspbian/pool/non-free/f/firmware-nonfree/firmware-brcm80211_0.43_all.deb
+dpkg -i firmware-brcm80211_0.43_all.deb
+
+for f in brcmfmac43430-sdio.bin brcmfmac43430-sdio.txt ; do
+	[ -f $f ] || curl -O https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/master/brcm80211/brcm/$f
+	cp $f /lib/firmware/brcm/
+done
+)
 
 # password is 'raspberry' 
 useradd -m pi -s /bin/bash -p '$6$24rIum07$C07au5jT1GDCaT7QIO4QpYfMmciSTEyeuhfNonEvM8E7NowPa1d2Gt9kMpxSnGJL1G/VvEXY2w6IA9FS.ipjF1'
